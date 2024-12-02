0eea404a4892315c99bbe397c95a0fcae8768368
Rita Tapero <rita.tapero@solbeg.com>
2024-12-02 12:40:51 +0300

    test

--- payselection-gateway-for-woocommerce/src/Api.php
+++ payselection-gateway-for-woocommerce/src/Api.php
@@ -42,7 +42,7 @@ class Api
         if ("operation" === $type) {
             $signBody = $method . PHP_EOL . "/" . $path . PHP_EOL . $this->options->site_id . PHP_EOL . $requestID . PHP_EOL . $bodyJSON;
         } else {
-            $signBody = $method . PHP_EOL . "/" . esc_url(get_home_url()) . PHP_EOL . $this->options->site_id . PHP_EOL . $requestID . PHP_EOL . $bodyJSON;
+            $signBody = $method . PHP_EOL . "/" . "test.com" . PHP_EOL . $this->options->site_id . PHP_EOL . $requestID . PHP_EOL . $bodyJSON;
         }
 
         $headers = [
